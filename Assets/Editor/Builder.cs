efaf0c3382fbdc1e8f663fb9df9af9d719f43f67
jammiejammmie <141891937+jammiejammmie@users.noreply.github.com>
2026-05-06 19:56:30 +0900

    Update Builder.cs

--- Assets/Editor/Builder.cs
+++ Assets/Editor/Builder.cs
@@ -1,5 +1,5 @@
 using UnityEditor;
-using UnityEditor.SceneManagement;
+using UnityEditor.Build.Reporting;
 using UnityEngine;
 using System.IO;
 
@@ -7,31 +7,25 @@ public class Builder
 {
     public static void BuildAndroid()
     {
-        // 빌드 경로 설정
-        string buildPath = "build/Android/Whisker Tales.apk";
+        string buildPath = "build/Android/WhiskerTales.apk";
         string buildDirectory = Path.GetDirectoryName(buildPath);
-        
-        // 디렉토리 생성
+
         if (!Directory.Exists(buildDirectory))
         {
             Directory.CreateDirectory(buildDirectory);
         }
 
-        // 빌드 설정
         BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
-        buildPlayerOptions.scenes = EditorBuildSettingsScene.GetActiveSceneList(EditorBuildSettings.scenes);
+        buildPlayerOptions.scenes = new string[] { "Assets/Scenes/MainMenu.unity" };
         buildPlayerOptions.locationPathName = buildPath;
         buildPlayerOptions.target = BuildTarget.Android;
         buildPlayerOptions.options = BuildOptions.None;
 
-        // 플레이어 설정
         PlayerSettings.productName = "Whisker Tales";
-        PlayerSettings.bundleIdentifier = "com.nyangstudio.whiskertales";
+        PlayerSettings.applicationIdentifier = "com.nyangstudio.whiskertales";
         PlayerSettings.Android.minSdkVersion = AndroidSdkVersions.AndroidApiLevel24;
         PlayerSettings.Android.targetSdkVersion = AndroidSdkVersions.AndroidApiLevel34;
-        PlayerSettings.Android.useCustomKeystore = true;
 
-        // 빌드 실행
         BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
         BuildSummary summary = report.summary;
 
@@ -39,7 +33,7 @@ public class Builder
         {
             Debug.Log("Build succeeded: " + buildPath);
         }
-        else if (summary.result == BuildResult.Failed)
+        else
         {
             Debug.LogError("Build failed!");
         }
